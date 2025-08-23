f3228ed24012320877a3db05f04b58b05620f46f
Chin-Hui Lee <voldemort92205@gmail.com>
2025-08-23 15:36:33 +0800

    [UPDATE] practice8: cwa data view: add rainfall station information
        - rainfall station is used in this view: rainfall strength level

--- practice8_opendataDemo/src/components/CWADatasetView.vue
+++ practice8_opendataDemo/src/components/CWADatasetView.vue
@@ -5,6 +5,8 @@ import { onMounted, reactive, ref } from 'vue';
 import { useCWAStore } from '../stores/useCWAStore.js';
 import SimpleSourceView from './SimpleSourceView.vue';
 import SimpleLoadingEffect from './SimpleLoadingEffect.vue';
+import SimpleProgressBarCircle from './SimpleProgressBarCircle.vue';
+import SimpleCircleMap from './SimpleCircleMap.vue';
 
 const cwaStore = useCWAStore();
 
@@ -44,14 +46,138 @@ const updateFigureLists = (timeStamp = "Null") => {
     postDownload(timeStamp);
 }
 
+const rainfallLevelDesc = [
+    "無雨： 24小時累積雨量為0毫米",
+    "小雨： 24小時累積雨量達0.01毫米以上 (自定義)",
+    "中雨： 24小時累積雨量達30毫米以上 (自定義)",
+    "大雨： 24小時累積雨量達80毫米以上",
+    "豪雨： 24小時累積雨量達200毫米以上",
+    "大豪雨： 24小時累積雨量達350毫米以上",
+    "超大豪雨： 24小時累積雨量達500毫米以上",
+]
+
+/* rainfall level
+量範圍（mm）	顏色代碼	顏色說明
+0            white     沒下雨
+0.001 - 30	#4575b4	藍色（小雨）
+30 - 80	#91cf60	綠色（中雨）
+80 - 200	#fee08b	黃色（大雨）
+200 - 350	#fc8d59	橘色（豪雨）
+350 - 500	#d73027	紅色（大豪雨）
+> 500	#7f0000	深紅（超大豪雨）
+*/
+
+const rainfallLevelClass = {
+    "無雨": {color: "#505050", level: 0},
+    "小雨": {color: "#4575b4", level: 0.001},
+    "中雨": {color: "#91cf60", level: 30},
+    "大雨": {color: "#fee08b", level: 80},
+    "豪雨": {color: "#fc8d59", level: 200},
+    "大豪雨": {color: "#d73027", level: 350},
+    "超大豪雨": {color: "#7f0000", level: 500},
+}
+
+const rainfallLevelLegend = Object.keys(rainfallLevelClass).map((key) => {
+    return {
+        "color": rainfallLevelClass[key].color,
+        "name": key,
+    };
+}).filter((item) => item.name !== "無雨")
+
+const rainfallLevelList = reactive({});
+const rainfallLevelMapInfo = reactive([]);
+const rainfallLevelProgressInfo = reactive([]);
+
+const handleRainfallLevel = () => {
+    const rainfallKey = "Past24hr";
+    const remoteData = cwaStore.dataSetRainfall[0].dataset;
+    const nData = remoteData.length
+
+    for (const key in rainfallLevelList)
+    {
+        delete rainfallLevelList[key];
+    }
+    for (const key in rainfallLevelClass)
+    {
+        const level = rainfallLevelClass[key];
+        rainfallLevelList[key] = reactive({
+            dataset:[],
+            class: key,
+            color: level.color,
+            totalCount: remoteData.length,
+        })
+    }
+
+    const reversedKey = Object.keys(rainfallLevelClass).reverse();
+    remoteData.forEach((item) => {
+        const rainfall = item.RainfallElement[rainfallKey].Precipitation;
+        let rainLevel = "無雨"
+        for (const key of reversedKey)
+        {
+            if (rainfall === 0.0)
+            {
+                break;
+            }
+            if (rainfall >= rainfallLevelClass[key].level)
+            {
+                rainLevel = key;
+                break;
+            }
+        }
+        rainfallLevelList[rainLevel]["dataset"].push({
+            StationName: item.StationName,
+            StationId: item.StationId,
+            GeoInfo: item.GeoInfo,
+            y: rainfall,
+        })
+    })
+
+    rainfallLevelMapInfo.splice(0, rainfallLevelMapInfo.length);
+    reversedKey.forEach((key) => {
+        if (key === "無雨")
+            return;
+        rainfallLevelList[key].dataset.forEach((item) => {
+            const obj = {};
+
+            obj["lat"] = item.GeoInfo.Coordinates[0].StationLatitude;
+            obj["lon"] = item.GeoInfo.Coordinates[0].StationLongitude;
+            obj["fillColor"] = rainfallLevelList[key].color;
+            obj["color"] = rainfallLevelList[key].color;
+            obj["color"] = "black"
+            obj["message"] = item.GeoInfo.CountyName + "/" + item.GeoInfo.TownName + " (rainfall: " + item.y + ")";
+            obj["radius"] = 5;
+            obj["weight"] = 1;
+
+            rainfallLevelMapInfo.push(obj)
+        })
+    })
+
+    rainfallLevelProgressInfo.splice(0, rainfallLevelProgressInfo.length);
+    reversedKey.forEach((key) => {
+        rainfallLevelProgressInfo.push({
+            "legend": key,
+            "value": rainfallLevelList[key].dataset.length,
+            "total": nData,
+            "color": rainfallLevelClass[key].color,
+        })
+    })
+}
+
+const updateRainfallList = (timeStamp = "Null") => {
+    handleRainfallLevel();
+};
+
 async function fetchDataSet(forceUpdate = false) {
     await cwaStore.initFetchSet(forceUpdate);
     updateFigureLists(cwaStore.refreshTimeFigures);
+    updateRainfallList(cwaStore.refreshTimeRainfall);
 }
 
 async function fetechFigures() {
     await cwaStore.fetchFigures(true);
+    await cwaStore.fetchRainfallData(true);
     updateFigureLists(cwaStore.refreshTimeFigures);
+    updateRainfallList(cwaStore.refreshTimeRainfall);
 }
 
 const refreshDataSet = (forceUpdate = false) => {
@@ -63,6 +189,7 @@ const toUpdateData = () => {
     preDownload();
     fetechFigures();
 }
+
 
 onMounted (() => {
     refreshDataSet();
@@ -100,6 +227,46 @@ onMounted (() => {
         </div>
         -->
 
+        <div class="my-5">
+            <div class="text-3xl font-semibold">
+                雨量觀測站 - 降雨分佈 和 降雨數量統計
+            </div>
+            <div class="grid grid-cols-1 lg:grid-cols-2 py-5">
+                <div class="h-140 w-120 mx-auto">
+                    <SimpleCircleMap
+                        :mapCenterLat="23.6"
+                        :mapCenterLon="121.05"
+                        :mapZoom = 7
+                        :mapCircleData=rainfallLevelMapInfo
+                        :mapLegends = rainfallLevelLegend
+                        mapId = "rainfallLevelContainer"
+                        class="border border-white"
+                        >
+                    </SimpleCircleMap>
+                </div>
+
+                <div class="grid grid-cols-3 md:grid-cols-2 xl:grid-cols-4 mx-auto my-5">
+                    <SimpleProgressBarCircle
+                        v-for="(item, key) in rainfallLevelProgressInfo" :key="key+'-progress'"
+                        :imageId="item.legend + '-progressbar-circle'"
+                        :currentProgress=item.value
+                        :totalProgress=item.total
+                        :title=item.legend
+                        :color=item.color
+                        class="mx-5 my-auto "
+                        >
+                    </SimpleProgressBarCircle>
+                </div>
+            </div>
+            <div class="my-5 text-left flex flex-col justify-center">
+                <div class="w-150 mx-auto">
+                <p>說明: 降雨強度為以過去 24hr 的累積雨量做分類。分類如下</p>
+                <ul class="list-disc list-inside">
+                    <li v-for="desc in rainfallLevelDesc">{{ desc }}</li>
+                </ul>
+                </div>
+            </div>
+        </div>
         <div v-for="(items, key) in figureLists" :key="key" :id="key"
                 class="my-5">
             <div class="text-3xl font-semibold">
@@ -113,7 +280,7 @@ onMounted (() => {
                         imgClass="h-95 p-2"
                         titleClass="text"
                         class="w-90 mx-auto rounded"
-                         />
+                />
             </div>
         </div>
         <SimpleSourceView :dataSrc="cwaStore.dataUrlSrc" />
---
+++ practice8_opendataDemo/src/components/SimpleProgressBarCircle.vue
@@ -0,0 +1,116 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+    imageId: {
+        type: String,
+        required: true,
+    },
+    currentProgress: {
+        type: Number,
+        required: true
+    },
+    totalProgress: {
+        type: Number,
+        required: true
+    },
+    width: {
+        type: Number,
+        default: 200,
+    },
+    height: {
+        type: Number,
+        default: 200,
+    },
+    title: {
+        type: String,
+        default: "",
+    },
+    size: {
+        type: Number,
+        default: 100,
+    },
+    strokeWidth: {
+        type: Number,
+        default: 12,
+    },
+    color: {
+        type: String,
+        default: '#4caf50',
+    },
+    bgColor: {
+        type: String,
+        default: '#e0e0e0',
+    },
+})
+
+const radius = computed(() => (props.size - props.strokeWidth) / 2);
+const circumference = computed(() => 2 * Math.PI * radius.value)
+
+const dashOffset = computed(() => {
+    let currentProgress = props.currentProgress;
+    let totalProgress = props.totalProgress;
+    if (currentProgress < 0)
+    {
+        currentProgress = 0;
+    }
+    else if (props.currentProgress > props.totalProgress)
+    {
+        currentProgress = totalProgress;
+    }
+    return circumference.value * (1 - currentProgress / totalProgress)
+})
+</script>
+
+<template>
+    <div class="" style="cursor: pointer;">
+        <div class="mx-auto">
+            <div class="text-lg my-2 mx-auto"> {{ props.title }}</div>
+            <svg :width="props.size" :height="props.size" :id="props.imageId">
+                <circle
+                    class="circle-bg"
+                    :r="radius"
+                    :cx="props.size / 2"
+                    :cy="props.size / 2"
+                    :stroke="props.bgColor"
+                    :stroke-width="props.strokeWidth"
+                    fill="none"
+                />
+
+                <circle
+                    class="circle-progress-bar"
+                    :r="radius"
+                    :cx="props.size / 2"
+                    :cy="props.size / 2"
+                    :stroke="props.color"
+                    :stroke-width="props.strokeWidth"
+                    fill="none"
+                    :stroke-dasharray="circumference"
+                    :stroke-dashoffset="dashOffset"
+                    stroke-linecap="round"
+                    transform="rotate(-90, 50, 50)"
+                />
+                <text
+                    x="50%"
+                    y="50%"
+                    text-anchor="middle"
+                    dominant-baseline="central"
+                    class="circle-text"
+                    >
+                    {{ props.currentProgress }}
+                </text>
+            </svg>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.circle-progress {
+  display: block;
+}
+
+.circle-text {
+  font-size: 1rem;
+  fill: #202020;
+}
+</style>
